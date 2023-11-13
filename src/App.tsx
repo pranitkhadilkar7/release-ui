eb0b60e5a523d88f2c0a549999256e43e21fae4d
Pranit Khadilkar <pranit@emtech.com>
2023-11-13 14:59:44 +0530

    added toast message handling for the apis calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { ToastContainer } from 'react-toastify'
 import { PageRoutes } from './routes/PageRoutes'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
-  return <PageRoutes />
+  return (
+    <>
+      <ToastContainer />
+      <PageRoutes />
+    </>
+  )
 }
 
 export default App
--- src/store/service.ts
+++ src/store/service.ts
@@ -1,10 +1,13 @@
+import { QueryReturnValue } from '@reduxjs/toolkit/dist/query/baseQueryTypes'
 import {
   BaseQueryFn,
   FetchArgs,
   FetchBaseQueryError,
+  FetchBaseQueryMeta,
   createApi,
   fetchBaseQuery,
 } from '@reduxjs/toolkit/query/react'
+import { toast } from 'react-toastify'
 
 const baseQuery = fetchBaseQuery({ baseUrl: 'http://localhost:3004/' })
 
@@ -13,8 +16,16 @@ const baseQueryWithToastImpl: BaseQueryFn<
   unknown,
   FetchBaseQueryError
 > = async (args, api, extraOptions) => {
-  let result = await baseQuery(args, api, extraOptions)
-  console.log(result)
+  let result = (await baseQuery(args, api, extraOptions)) as QueryReturnValue<
+    any,
+    { status: number; data: any },
+    FetchBaseQueryMeta
+  >
+  if (result?.data?.message) {
+    toast.success(result.data.message)
+  } else if (result?.error?.data?.message) {
+    toast.error(result?.error?.data?.message)
+  }
   return result
 }
 
