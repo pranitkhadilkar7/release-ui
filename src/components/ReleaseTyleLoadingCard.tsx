d3af03b5316f2e4ee36b45e360306e826a5aa5ab
Pranit Khadilkar <pranit@emtech.com>
2023-11-27 10:29:45 +0530

    added loading component for the release card

---
+++ src/components/ReleaseTyleLoadingCard.tsx
@@ -0,0 +1,13 @@
+export function ReleaseTypeLoadingCard() {
+  return (
+    <div
+      className={`tw-min-h-112.5 tw-max-w-full tw-min-w-50 md:tw-max-w-125 md:tw-min-w-125 tw-bg-guyabano 
+    tw-m-5 tw-p-10 tw-rounded-2xl tw-border-2 tw-border-solid tw-border-guyabano hover:tw-border-primary
+    tw-flex tw-flex-col`}
+    >
+      <div className="tw-mb-5 tw-h-8 tw-rounded-lg tw-bg-gray-300 tw-animate-pulse"></div>
+      <div className="tw-mb-5 tw-h-6 tw-rounded-lg tw-bg-gray-300 tw-animate-pulse"></div>
+      <div className="tw-mb-5 tw-h-12 tw-w-28 tw-rounded-lg tw-bg-primary tw-animate-pulse"></div>
+    </div>
+  )
+}
